380137b608454882069bd16b3e1b89b66429e5f8
pragathiyp <90029094+pragathiyp@users.noreply.github.com>
2022-03-16 13:35:03 +0530

    Create generic.java

---
+++ generic.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Gen<T, V> {
+
+T ob1;
+V ob2;
+
+Gen(T x, V y) {
+ob1 = x;
+ob2 = y;
+}
+
+T getob1() {
+return ob1;
+}
+
+V getob2() {
+return ob2;
+}
+
+void showType() {
+System.out.println("Type of T =" + ob1.getClass().getName());
+System.out.println("Type of V =" + ob2.getClass().getName());
+}
+}
+
+class generics {
+
+public static void main(String args[]) {
+Gen<Integer, String> wob;
+wob = new Gen<Integer, String>(250, "BMS College Of Engineering");
+int x = wob.getob1();
+System.out.println("The value for x =" + x);
+String y = wob.getob2();
+System.out.println("The value for y =" + y);
+wob.showType();
+}
+}
